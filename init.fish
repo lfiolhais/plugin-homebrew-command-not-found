7f3550d2b4c56e76d4a85dd8bcffe99f4c5580fa
Luís Fiolhais <luisazenhas.fiolhais@gmail.com>
2015-12-07 21:58:48 +0000

    Run command

--- init.fish
+++ init.fish
@@ -4,5 +4,11 @@ function init -a path --on-event init_homebrew-command-not-found
     return 1
   end
 
-  
+  if not available brew command command-not-found-init
+    echo "Please install 'hombrew-command-not-found' first"
+    return 1
+  end
+
+  brew command command-not-found-init > /dev/null; and . (brew command-not-found-init)
+
 end
