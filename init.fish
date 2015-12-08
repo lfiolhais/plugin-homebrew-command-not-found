26781a470fce22d89c0c45c9188193c20af1629d
Luís Fiolhais <luisazenhas.fiolhais@gmail.com>
2015-12-08 11:51:58 +0000

    Check if brew_cmd is set and run command

--- init.fish
+++ init.fish
@@ -6,12 +6,12 @@ function init -a path --on-event init_homebrew-command-not-found
 
   set -l brew_cmd (command brew command command-not-found-init 2> /dev/null)
 
-  if test -eq status 1
+  if [ -n "$brew_cmd" ]
+    brew command command-not-found-init > /dev/null; and . (brew command-not-found-init)
+  else
     echo "Please install 'hombrew-command-not-found' first."
     echo "Check out https://github.com/Homebrew/homebrew-command-not-found"
     return 1
   end
-
-  brew command command-not-found-init > /dev/null; and . (brew command-not-found-init)
 
 end
