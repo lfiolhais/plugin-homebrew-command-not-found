1162bb790a8bf98db11148bc5fda20aecd63d473
Justin Hileman <justin@justinhileman.info>
2016-03-24 00:58:32 -0700

    Improve compatibility with other frameworks and future OMF.
     * Initialize immediately, rather than waiting for an event.
     * Replace `available` usage with the more idiomatic `type -q`.

--- init.fish
+++ init.fish
@@ -1,9 +1,4 @@
-function init -a path --on-event init_homebrew-command-not-found
-  if not available brew
-    echo "Please install 'brew' first"
-    return 1
-  end
-
+if type -q brew
   set -l brew_cmd (command brew command command-not-found-init 2> /dev/null)
 
   if [ -n "$brew_cmd" ]
@@ -11,7 +6,7 @@ function init -a path --on-event init_homebrew-command-not-found
   else
     echo "Please install 'hombrew-command-not-found' first."
     echo "Check out https://github.com/Homebrew/homebrew-command-not-found"
-    return 1
   end
-
+else
+  echo "Please install 'brew' first"
 end
